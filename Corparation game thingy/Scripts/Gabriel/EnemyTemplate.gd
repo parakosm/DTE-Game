5939c299242d97e78481a8b495bc04fb92999f97
rendleg2 <20136@rosmini.school.nz>
2025-02-19 21:41:06 +1300

    Added The skelton for the Enemy Template
    Added the Skelton of the functions needed
    Added Some code to the AI for testing perpisos
    File Mangment
    Added Required Nodes

--- Corparation game thingy/Scripts/Gabriel/EnemyTemplate.gd
+++ Corparation game thingy/Scripts/Gabriel/EnemyTemplate.gd
@@ -1,11 +1,24 @@
 extends Node
-
-
+var HP = 100
+signal Health_Changed
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Hit()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func Pathfind():
+	pass #PathFind To Cords Specified by BeeHaveTree
+
+func Shoot():
+	pass #Shoot Spot Specified by BeeHaveTree
+	
+func Face():
+	pass #Face Derection spesifide by Other Functions AND Swivile as spesifide by BeeHaveTree
+
+func Hit():
+	Health_Changed.emit(HP)
