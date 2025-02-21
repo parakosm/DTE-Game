a50c1cffced4a18c09d0aac2b43e06cdf2be9da8
rendleg2 <20136@rosmini.school.nz>
2025-02-22 12:10:39 +1300

    Added Basic Pathfinding to Enemy Template
    Enemy Templates Now follow the curser
    Still need to make it pathfind around walls

--- Corparation game thingy/Scripts/Gabriel/EnemyTemplate.gd
+++ Corparation game thingy/Scripts/Gabriel/EnemyTemplate.gd
@@ -1,15 +1,32 @@
-extends Node
+extends CharacterBody2D
+
 var HP = 100
 signal Health_Changed
+var Target_Position
+var Current_Position
+var Movement_Speed = 50
+var Next_Step
+var Velocity
+@onready var navigation_agent_2d = $NavigationAgent2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Hit()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+func _physics_process(delta: float) -> void:
+	Target_Position = get_global_mouse_position()
+	navigation_agent_2d.target_position = Target_Position
+	Current_Position = self.global_position
+	Next_Step = navigation_agent_2d.get_next_path_position()
+	Velocity = Current_Position.direction_to(Next_Step) * Movement_Speed
+	
+	if navigation_agent_2d.avoidance_enabled:
+		navigation_agent_2d.set_velocity(Velocity)
+	else:
+		_on_navigation_agent_2d_velocity_computed(Velocity)
+	move_and_slide()
 
 func Pathfind():
 	pass #PathFind To Cords Specified by BeeHaveTree
@@ -22,3 +39,7 @@ func Face():
 
 func Hit():
 	Health_Changed.emit(HP)
+
+
+func _on_navigation_agent_2d_velocity_computed(safe_velocity):
+	velocity = safe_velocity
