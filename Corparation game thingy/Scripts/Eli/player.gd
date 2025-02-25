0336ec356506b433234fe291a0d46f5c37cb75d6
parakosm <161525773+parakosm@users.noreply.github.com>
2025-02-26 10:00:36 +1300

    Worked on player, movement, added sprint mechanics

--- Corparation game thingy/Scripts/Eli/player.gd
+++ Corparation game thingy/Scripts/Eli/player.gd
@@ -1,8 +1,12 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var speed = 42 # Nobody change this variable
 
 func get_input():
+	if Input.is_action_pressed("shift"):
+		var speed = 69  
+	else:
+		var speed = 42
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 
