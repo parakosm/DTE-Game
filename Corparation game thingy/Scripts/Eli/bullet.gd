d99e688296de3a6ae1fdfb8bdfeb2e6dd43bca33
parakosm <161525773+parakosm@users.noreply.github.com>
2025-03-13 14:21:23 +1300

    Bullets Finally Working
    Now we just need to make them hit enemies and reduce health! Finish is in sight.

--- Corparation game thingy/Scripts/Eli/bullet.gd
+++ Corparation game thingy/Scripts/Eli/bullet.gd
@@ -1,11 +1,11 @@
-extends Node2D
+extends Area2D
 
-var speed = 750
+var speed = 42
 
 func _physics_process(delta):
 	position += transform.x * speed * delta
 
-func _on_Bullet_body_entered(body):
+func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("enemy"):
 		body.queue_free()
 	queue_free()
