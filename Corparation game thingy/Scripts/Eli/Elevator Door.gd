d819ad697610e9dbb77f6368c12461f1721a3bf8
parakosm <161525773+parakosm@users.noreply.github.com>
2025-03-25 13:57:08 +1300

    Got Elevator Door Working
    Switches to next level when elevator is entered and to end screen when there are no more levels. Next task is to globalise XP and require Keycards to enter the elevator.

--- Corparation game thingy/Scripts/Eli/Elevator Door.gd
+++ Corparation game thingy/Scripts/Eli/Elevator Door.gd
@@ -3,5 +3,8 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	print("Elevator Door Entered")
 	Global.GlobalLevel += 1
-	print(str("res://Scenes/level ", Global.GlobalLevel, ".tscn"))
-	get_tree().change_scene_to_file(str("res://level ", Global.GlobalLevel, ".tscn"))
+	if Global.GlobalLevel == 3:
+		get_tree().change_scene_to_file(str("res://End Screen.tscn"))
+	else:
+		print(str("res://Scenes/level ", Global.GlobalLevel, ".tscn"))
+		get_tree().change_scene_to_file(str("res://Scenes/level ", Global.GlobalLevel, ".tscn"))
