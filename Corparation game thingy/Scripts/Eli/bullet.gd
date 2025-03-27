d195f982e77153f09a9837f17d52c0fe518e51a8
parakosm <161525773+parakosm@users.noreply.github.com>
2025-03-28 10:19:29 +1300

    Player Death Working
    It turns out player death. Added a comment so I could make this commit for completeness.

--- Corparation game thingy/Scripts/Eli/bullet.gd
+++ Corparation game thingy/Scripts/Eli/bullet.gd
@@ -5,7 +5,7 @@ var speed = 123
 func _physics_process(delta):
 	position += transform.x * speed * delta
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(body: Node2D) -> void: # On a hit, the bullet looks to see if the object collided into does anything when hit and if so runs that function, before destroying it
 	if body.has_method("Hit"):
 		body.Hit()
-	queue_free()
+	queue_free() 
